f810eb093885dcc9ab818301471c94b245689de0
AhmednThabet <ah12.9.2000@gmail.com>
2022-10-13 18:57:37 +0300

    Navbar proto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
-import './App.css';
-import Detail from './Components/Products/Detailes/Detail';
-import Products from './Pages/Products.js';
-
+import "./App.css";
+import Detail from "./Components/Products/Detailes/Detail";
+import Products from "./Pages/Products.js";
+import ProductsSecondView from "./Pages/products-secondView/ProductsSecondView";
+import Nav from "./Components/NavBar/Nav";
 function App() {
   return (
     <div className="App">
-        <Products></Products> 
+      <Nav />
+      <ProductsSecondView />
+      {/* <Products></Products>  */}
       {/* <Detail/> */}
     </div>
   );
--- src/Components/NavBar/Nav.js
+++ src/Components/NavBar/Nav.js
@@ -1,8 +1,36 @@
-// add your component
 import React from "react";
+import "./Nav.css";
+import { IoCart } from "react-icons/io5";
+import { MdPersonAddAlt1 } from "react-icons/md";
 
-const Nav = () => {
-  return <div>Nav</div>;
-};
+function Nav() {
+  return (
+    <div className="NavBar">
+      <div className="logo">
+        <img
+          src="https://react-course-comfy-sloth-store.netlify.app/static/media/logo.221f6b13.svg"
+          alt="#"
+        />
+      </div>
+      <div className="links">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Products</li>
+        </ul>
+      </div>
+      <div className="cart-logIn">
+        <div className="cart">
+          Cart
+          <IoCart />
+        </div>
+        <div className="login">
+          Login
+          <MdPersonAddAlt1 />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Nav;
