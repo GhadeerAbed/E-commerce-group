16b6ab007b4de6b92a9b45ae5e900937e88da7aa
AhmednThabet <ah12.9.2000@gmail.com>
2022-10-15 14:39:48 +0300

    resolve conflicts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
-import Detail from "./Components/Products/Detailes/Detail";
-import Products from "./Pages/Product/Products.js";
-import ProductsSecondView from "./Pages/products-secondView/ProductsSecondView";
-import  About  from '../src/Components/About/About.js'
-import Test1 from '../src/Components/Cart/Test1.js'
-import Checkout from './Components/checkout/Checkout.js'
-import Featured from './Components/Featured/index.js'
-import Footer from './Components/Footer/Footer.js'
-import HeaderNav  from './Components/HeaderNav/HeaderNav.js'
-import Nav from './Components/NavBar/Nav.js'
-import Show from './Components/show/index.js'
-import Home from './Components/Home/Home.js';
+// import Detail from "./Components/Products/Detailes/Detail";
+// import Products from "./Pages/Product/Products.js";
+// import ProductsSecondView from "./Pages/products-secondView/ProductsSecondView";
+// import  About  from '../src/Components/About/About.js'
+// import Test1 from '../src/Components/Cart/Test1.js'
+// import Checkout from './Components/checkout/Checkout.js'
+// import Featured from './Components/Featured/index.js'
+// import Footer from './Components/Footer/Footer.js'
+// import HeaderNav  from './Components/HeaderNav/HeaderNav.js'
+// import Nav from './Components/NavBar/Nav.js'
+// import Show from './Components/show/index.js'
+// import Home from './Components/Home/Home.js';
 function App() {
   return (
     <div className="App">
@@ -32,7 +32,7 @@ function App() {
       betwee them will change only 
       */}
   
-       <Detail />
+       {/* <Detail /> */}
     </div>
   );
 }
