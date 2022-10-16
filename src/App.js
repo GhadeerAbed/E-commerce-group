49b104b6dd26ae97d8c360bf043088a6d7d29541
shaymaa <shaymaa@gmail.com>
2022-10-16 19:16:43 +0300

    ShSho

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 
-import React from 'react'
 // import Detail from "./Components/Products/Detailes/Detail";
 // import Products from "./Pages/Product/Products.js";
 // import ProductsSecondView from "./Pages/products-secondView/ProductsSecondView";
 // import  About  from '../src/Components/About/About.js'
-//  import Test1 from '../src/Components/Cart/Test1.js'
+// import Test1 from '../src/Components/Cart/Test1.js'
 // import Checkout from './Components/checkout/Checkout.js'
 //import Featured from './Components/Featured/index.js'
 // import Footer from './Components/Footer/Footer.js'
 // import HeaderNav  from './Components/HeaderNav/HeaderNav.js'
 // import Nav from './Components/NavBar/Nav.js'
 // import Show from './Components/show/index.js'
-//  import Home from './Components/Home/Home.js';
-
+// import Home from './Components/Home/Home.js';
 const App = () => {
   return (
   <>
-  
-  
-    {/* <ProductsSecondView /> */}
+   {/* <ProductsSecondView /> */}
         {/* <Products></Products>    */}
         {/* <Detail/> */}
-        {/*   <Test1></Test1> */}
+        {/* <Test1></Test1>  */}
         {/* <Checkout></Checkout> */}
         {/* <About ></About> */}
         {/* <Featured></Featured>  */}
         {/* <Show></Show> */}
-        {/*  <Home></Home>  */}
-       
+        {/* <Home></Home>  */}
+        
          {/* <Nav></Nav>  
          <HeaderNav ></HeaderNav>     
          <Footer></Footer>  */}
+
+
   </>
   )
 }
