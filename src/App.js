254fd054087887ab0748dfe163b5ae567251190d
GhadeerAbed <ghadfannana@gmail.com>
2022-10-16 17:37:32 +0300

    third update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 // import  About  from '../src/Components/About/About.js'
 // import Test1 from '../src/Components/Cart/Test1.js'
 // import Checkout from './Components/checkout/Checkout.js'
-// import Featured from './Components/Featured/index.js'
+//import Featured from './Components/Featured/index.js'
 // import Footer from './Components/Footer/Footer.js'
 // import HeaderNav  from './Components/HeaderNav/HeaderNav.js'
 // import Nav from './Components/NavBar/Nav.js'
@@ -14,25 +14,21 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      {/* <ProductsSecondView /> */}
-      {/* <Products></Products>    */}
-      {/* <Detail/> */}
-      {/* <Test1></Test1>    //css had changed and unformatted */}
-      {/* <Checkout></Checkout> */}
-      {/* <About ></About> */}
-      {/* //css had changed and unformatted */}
-      {/* <Featured></Featured>  //there is a big error data.json is missed */}
-      {/* <Show></Show> */}
-      {/* <Home></Home> //css had changed and unformatted  */}
-      {/* /****************************************************************** */}
-      {/* <Nav></Nav> 
-      <HeaderNav ></HeaderNav>    */}
-      {/* <Footer></Footer>    
-      just Those components should be in the app 
-      betwee them will change only 
-      */}
+      <>
+        {/* <ProductsSecondView /> */}
+        {/* <Products></Products>    */}
+        {/* <Detail/> */}
+        {/* <Test1></Test1>  */}
+        {/* <Checkout></Checkout> */}
+        {/* <About ></About> */}
+        {/* <Featured></Featured>  */}
+        {/* <Show></Show> */}
+        {/* <Home></Home>  */}
 
-      {/* <Detail /> */}
+        {/* <Nav></Nav>  */}
+        {/* <HeaderNav ></HeaderNav>     */}
+        {/* <Footer></Footer> */}
+      </>
     </div>
   );
 }
