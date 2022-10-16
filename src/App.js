ab3a768d2186177405513abe1f1cd4434c1d3df0
shaymaa <shaymaa@gmail.com>
2022-10-16 04:40:40 +0300

    Shoman

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import "./App.css";
 //import Products from "./Pages/Product/Products.js";
 //import ProductsSecondView from "./Pages/products-secondView/ProductsSecondView";
 // import  About  from '../src/Components/About/About.js'
-import Test1 from '../src/Components/Cart/Test1.js'
+// import Test1 from '../src/Components/Cart/Test1.js'
 // import Checkout from './Components/checkout/Checkout.js'
 //import Featured from './Components/Featured/index.js'
 //import Footer from './Components/Footer/Footer.js'
 //import HeaderNav  from './Components/HeaderNav/HeaderNav.js'
 //import Nav from './Components/NavBar/Nav.js'
 // import Show from './Components/show/index.js'
-import Home from './Components/Home/Home.js';
+//  import Home from './Components/Home/Home.js';
 //import Detail  from './Components/Products/Detailes/Detail.js'
 function App() {
   return (
@@ -18,15 +18,15 @@ function App() {
       {/* <ProductsSecondView /> */}
       {/* <Products></Products>    */}
       {/* <Detail/> */}
-      {/*  <About/>  <Test1/> */}
-       
+      {/*  <About/>   <Test1/> */}
+       {/* <Home /> */}
+     
       {/* <Checkout></Checkout> */}
       {/* <Featured></Featured>  //there is a big error data.json is missed */} 
       {/* <Footer></Footer>      */}
       {/* <HeaderNav ></HeaderNav> */}
       {/* <Nav></Nav> */}
       {/* <Show></Show> */}
-       {/* <Home /> */}
     
       {/* <Detail ></Detail> */}
     </div>
